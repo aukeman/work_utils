d532df98cc4f0d8f5e17864880bb36c29b670095
Jake Aukeman <jaukeman@virtualhold.com>
2014-07-11 13:35:52 -0400

    added usage string.  when looking for branches, consider branches on remotes as well

--- list_repos.sh
+++ list_repos.sh
@@ -7,19 +7,31 @@ diff=false
 branch=
 remote=origin/master
 
-
-
-
 spinner=( '|' '/' '-' '\' ) 
+
+USAGE="Perform various queries of git repositories under \$GIT_REPO_ROOT.
+
+Usage:
+  $(basename ${0}) [-s] [-d | -u | -r <remote branch>] ] [-b <branch name pattern>] | -h
+
+  -s: show status (git status -sb) for the repositories returned by the query
+
+  -d: show repositories with diffs between the current working branch and origin/master
+  -u: show repositories with diffs between the current working branch and the branch's upstream remote
+  -r: show repositories with diffs between the current working branch and the given remote branch
+
+  -b: show repositories with local or remote branches matching the given pattern
+"
 
 while getopts "hdsub:r:" opt; do
   case ${opt} in
     d) diff=true;;
     s) status=true;;
     b) branch=${OPTARG};;
-    r) remote=${OPTARG};;
-    u) remote='@{u}';;
-    h) echo "TODO"; exit 0;;
+    r) remote=${OPTARG}; diff=true;;
+    u) remote='@{u}'; diff=true;;
+    h) echo "$USAGE"; exit 0;;
+    *) echo "$USAGE" >&2; exit 1;;
   esac
 done
 
@@ -38,7 +50,7 @@ if ${diff}; then
   echo "Listing repos with diffs between working tree and ${remote/@\{u\}/upstream remote}"
   echo
 elif [[ -n ${branch} ]]; then
-  echo "Listing repos containing a branch matching \"${branch}\""
+  echo "Listing repos containing a local or remote branch matching \"${branch}\""
   echo
 fi
 
@@ -49,15 +61,15 @@ for repo in $(find ${GIT_REPO_ROOT:-~/git/} -name .git -a -type d -mindepth 2 -m
   pushd ${dir} >/dev/null
 
   if ( ${list} ) ||
-     ( [[ -n ${branch} ]] && git branch --list | grep --quiet ${branch} ) ||
+     ( [[ -n ${branch} ]] && git branch --list --all | grep --quiet ${branch} ) ||
      ( [[ -z ${branch} ]] && git branch --list --remote | grep --quiet ${remote} && ! git diff --quiet ${remote}.. ); then
 
     echo -en ${cr}
     echo $(basename ${dir}) \($(git branch | grep "^\*" | cut -c3-)\)
 
     if [[ -n ${branch} ]] && 
-       (( 1 < $(git branch --list | grep ${branch} | wc -l) )); then
-      git branch --list | grep ${branch}
+       (( 1 < $(git branch --list --all | grep ${branch} | wc -l) )); then
+      git branch --list | grep ${branch} | tr "*" " "
       echo
     fi 
 
