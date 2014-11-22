08c8124bb885cd8c4a4df905612b832da3afc99a
Jake Aukeman <jaukeman@virtualhold.com>
2014-11-22 09:53:45 -0500

    use ruby for card number regexes

--- hooks/fightclub_git_plugin/common.sh
+++ hooks/fightclub_git_plugin/common.sh
@@ -3,20 +3,23 @@
 # PIVOTAL_PROJECT_ID="985502"
 # PIVOTAL_TOKEN="5d089c9f3dc01c734dece727cc65e513";
 
-
 if [[ -z ${PIVOTAL_PROJECT_ID} ]]; then
-	echo "\$PIVOTAL_PROJECT_ID is not defined" >&2
+	echo "\$PIVOTAL_PROJECT_ID environment variable is not defined" >&2
 	exit 1
 elif [[ -z ${PIVOTAL_TOKEN} ]]; then
-	echo "\$PIVOTAL_TOKEN is not defined" >&2
+	echo "\$PIVOTAL_TOKEN environment variable is not defined" >&2
 	exit 1
 fi
 
-OPTIONAL_NUMBER_MARKER_SED_REGEX='[\[{(]\{0,1\}#\{0,1\}'
-OPTIONAL_NUMBER_MARKER_EGREP_REGEX='[\[\{(]{0,1}#{0,1}'
+OPTIONAL_NUMBER_MARKER_REGEX='\[?#?'
+PIVOTAL_CARD_NUMBER_REGEX='\d{8,}'
 
-PIVOTAL_CARD_NUMBER_SED_REGEX='[0-9]\{8,\}'
-PIVOTAL_CARD_NUMBER_EGREP_REGEX='[0-9]{8,}'
+function generate_error_message
+{
+	echo
+	echo "Failed to ${1}."
+	echo "If you need to bypass the Fight Club git hooks, run this command again and pass the --no-verify option."
+}
 
 function get_branch_name()
 {
@@ -25,21 +28,17 @@ function get_branch_name()
 
 function is_pivotal_card_number_in_string()
 {
-	echo "${1}" | egrep -q "${OPTIONAL_NUMBER_MARKER_EGREP_REGEX}${PIVOTAL_CARD_NUMBER_EGREP_REGEX}"
+	ruby -e 'if not ARGV[0] =~ /#{ARGV[1]}/; then exit 1; end' -- "${1}" "${PIVOTAL_CARD_NUMBER_REGEX}"
 }
 
 function does_pivotal_card_number_begin_string()
 {
-	echo "${1}" | egrep -q "^${OPTIONAL_NUMBER_MARKER_EGREP_REGEX}${PIVOTAL_CARD_NUMBER_EGREP_REGEX}"
+	ruby -e 'if not ARGV[0] =~ /^#{ARGV[1]}/; then exit 1; end' -- "${1}" "${OPTIONAL_NUMBER_MARKER_REGEX}${PIVOTAL_CARD_NUMBER_REGEX}"
 }
 
 function get_pivotal_card_number_from_string()
 {
-	if is_pivotal_card_number_in_string "${1}"; then
-		echo "${1}" | sed -e "s/[^0-9]*\(${PIVOTAL_CARD_NUMBER_SED_REGEX}\).*/\1/"
-	else
-		echo
-	fi
+	ruby -e 'ARGV[0] =~ /(#{ARGV[1]})/; print $1' -- "${1}" "${PIVOTAL_CARD_NUMBER_REGEX}"
 }
 
 function get_pivotal_card_number_from_commit_message_file()
